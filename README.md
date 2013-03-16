cf019e164f563f869ef0ce456a5af1fe4cc531c1
Alex Baranosky <alexander.baranosky@gmail.com>
2013-03-16 13:58:31 -0700

    added transaction rollback test; added rollback example to README

--- README.md
+++ README.md
@@ -16,18 +16,38 @@ Hazel Refs
 (def hazelcast (make-hazelcast))
 
 (def r1 (hazel-ref hazelcast "my-ref" 1))
-(is (= 1 @r1))
+;; @r1 => 1
 
 (def r2 (hazel-ref hazelcast "my-ref" 2))
-(is (= 2 @r1 @r2))
+;; @r1 => 2
+;; @r2 => 2
 
 (do-transaction hazelcast
                 (halter r1 + 3 4))
-(is (= 9 @r1 @r2))
+;; @r1 => 9
+;; @r2 => 9
 
 (do-transaction hazelcast
-                (hazel-ref-set r2 99))
-(is (= 99 @r1 @r2))
+                (hazel-ref-set r2 88))
+;; @r1 => 88
+;; @r2 => 88
+```
+
+`dostransaction` can roll-back
+
+```clj
+(def  r1 (hazel-ref hazelcast "my-ref1" 1))
+;; @r1 => 1
+
+(def r2 (hazel-ref hazelcast "my-ref2" 2))
+;; @r2 => 2
+
+(do-transaction hazelcast
+                (halter r1 + 3 4)
+                (throw (Exception. "note, no changes to r1 or r2"))
+                (halter r2 +99))
+;; @r1 => 1
+;; @r2 => 2
 ```
 
 Hazel Caches
--- test/witch_hazel/test/hazel_ref.clj
+++ test/witch_hazel/test/hazel_ref.clj
@@ -9,15 +9,45 @@
 (deftest all-public-vars-have-docstrings
   (is (= [] (remove (comp :doc meta) (vals (ns-publics 'witch-hazel.hazel-ref))))))
 
-(deftest test-hazel-ref
+(deftest test-basic-hazel-ref
   (let [r1 (hazel-ref hazelcast "my-ref" 1)
         _  (is (= 1 @r1))
         r2 (hazel-ref hazelcast "my-ref" 2)
         _  (is (= 2 @r1 @r2))
         _ (do-transaction hazelcast
-                           (halter r1 + 3 4))
+                          (halter r1 + 3 4))
         _ (is (= 9 @r1 @r2))
         _ (do-transaction hazelcast
                           (hazel-ref-set r2 99))
         _ (is (= 99 @r1 @r2))]))
 
+(deftest test-transactions
+  (let [r1 (hazel-ref hazelcast "my-ref1" 1)
+        _  (is (= 1 @r1))
+        r2 (hazel-ref hazelcast "my-ref2" 2)]
+
+    (is (= 2 @r2))
+
+    (do-transaction hazelcast
+                    (halter r1 + 3 4)
+                    (throw (Exception. "note, no changes to r1 or r2"))
+                    (halter r2 +99))
+    (is (= 1 @r1))
+    (is (= 2 @r2))))
+
+
+
+
+
+(def  r1 (hazel-ref hazelcast "my-ref1" 1))
+(is (= 1 @r1))
+(def r2 (hazel-ref hazelcast "my-ref2" 2))
+
+(is (= 2 @r2))
+
+(do-transaction hazelcast
+                (halter r1 + 3 4)
+                (throw (Exception. "note, no changes to r1 or r2"))
+                (halter r2 +99))
+(is (= 1 @r1))
+(is (= 2 @r2))
